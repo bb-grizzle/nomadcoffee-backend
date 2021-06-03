aee0729a286d5172684008d4057ecd9535a2c8e9
bb-grizzle <firbigi1993@gmail.com>
2021-06-03 15:34:03 +0900

    fix schema extension type

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,7 @@
 import { mergeTypeDefs, mergeResolvers, loadFilesSync } from "graphql-tools";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.*`);
 export const typeDefs = mergeTypeDefs(loadedTypes);
 
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.ts`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.*`);
 export const resolvers = mergeResolvers(loadedResolvers);
